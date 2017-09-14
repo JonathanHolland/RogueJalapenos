4c999b41e0ee8bc7f7742b110f3c1e2db11e5aff
Jonathan Holland <jonathan.holland8@gmail.com>
2017-09-14 20:24:40 +1000

    Adjusting styles and using library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ApolloClient, createNetworkInterface, gql, graphql, ApolloProvider } from 'react-apollo';
+import Sidebar from 'react-sidebar';
 import './App.css';
 
 const networkInterface = createNetworkInterface({
@@ -62,17 +63,70 @@ const CustomersList = ({ data: {loading, error, customers }}) => {
 
 const CustomersListWithData = graphql(customersListQuery)(CustomersList);
 
+const mql = window.matchMedia(`(min-width: 800px)`);
+
+const sidebarStyles = {
+    sidebar: {
+        backgroundColor: '#577492',
+        opacity: '20%', 
+        width: '300px'
+    }
+}
+
+const mainStyles = {
+   margin: '40px'
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mql: mql,
+      docked: props.docked,
+      open: props.open
+    }
+
+    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  onSetSidebarOpen(open) {
+    this.setState({sidebarOpen: open});
+  }
+
+  componentWillMount() {
+    mql.addListener(this.mediaQueryChanged);
+    this.setState({mql: mql, sidebarDocked: mql.matches});
+  }
+
+  componentWillUnmount() {
+    this.state.mql.removeListener(this.mediaQueryChanged);
+  }
+
+  mediaQueryChanged() {
+    this.setState({sidebarDocked: this.state.mql.matches});
+  }
+  
   render() {
+    var sidebarContent = <b>Sidebar content</b>;
+
     return (
-      <ApolloProvider client={client}>
-      <div className="App">
-        <div className="App-header">
-        <h2>Welcome</h2>
-        </div>
-        <CustomersListWithData />
-      </div>
-      </ApolloProvider>
+        <Sidebar styles={sidebarStyles} 
+               sidebar={sidebarContent}
+               open={this.state.sidebarOpen}
+               docked={this.state.sidebarDocked}
+               onSetOpen={this.onSetSidebarOpen}>
+          <ApolloProvider client={client}>
+          <div className="App" style={mainStyles}>
+            <div className="App-header">
+            <h2>Transaction Account - 3331 2349</h2>
+            </div>
+            <CustomersListWithData />
+          </div>
+          </ApolloProvider>
+        </Sidebar>
+
     );
   }
 }
